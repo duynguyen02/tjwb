6e54be1bc6d247c48d8d1feae9e21661e63c3ac8
Duy Nguyen <duynguyen02.dev@gmail.com>
2024-11-11 20:40:54 +0700

    all columns constants of dataframe

---
+++ tjwb/columns_constant.py
@@ -0,0 +1,16 @@
+# Base Data
+TIME_SERIES = 'TimeSeries'
+WATER_LEVEL = 'WaterLevel'
+PUMP = 'Pump'
+BOX_CULVERT = 'BoxCulvert'
+VALVE_OVERFLOW = 'ValveOverflow'
+CAPACITY = 'Capacity'
+INTERVAL = 'Interval'
+
+# Q Out Data
+OUTFLOW = 'Outflow'
+OUT_BOX_CULVERT = f'Out.{BOX_CULVERT}'
+OUT_VALVE_OVERFLOW = f'Out.{VALVE_OVERFLOW}'
+
+# Q In Data
+INFLOW = 'Inflow'
